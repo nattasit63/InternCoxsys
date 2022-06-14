a2daa0ae9a5e194c44db3ba0c661a99f1a066112
nattasit63 <79138906+nattasit63@users.noreply.github.com>
2022-06-14 22:32:38 +0700

    run RVIZ2 done

--- interncoxsys/launch/qq.launch.py
+++ interncoxsys/launch/qq.launch.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import os
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
@@ -7,6 +8,9 @@ from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
 from ament_index_python.packages import get_package_share_directory
 import xacro
+from launch_ros.actions import Node
+import launch_ros.actions
+from launch import LaunchDescription
 def generate_launch_description():
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='True')
@@ -23,6 +27,7 @@ def generate_launch_description():
     
     robot_state_publisher_node = Node(package="robot_state_publisher", executable="robot_state_publisher",output="both",parameters=[robot_description],)
 
+
     world = os.path.join(pkg_dir, 'world', world_file_name)
    
     gazebo = ExecuteProcess(cmd=['gazebo', '--verbose', world, '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],output='screen')
@@ -31,13 +36,26 @@ def generate_launch_description():
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'Coconut'],
                         output='screen')
-  
+
+    package_description = "interncoxsys"
+
+    # RVIZ Configuration
+    rviz_config_dir = os.path.join(get_package_share_directory(
+        package_description), 'rviz', 'camera00.rviz')
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        name='rviz_node',
+        parameters=[{'use_sim_time': True}],
+        arguments=['-d', rviz_config_dir])
+ 
     return LaunchDescription([
         
-        robot_state_publisher_node ,
         spawn_entity,
         gazebo,
-       
-        
+        robot_state_publisher_node ,
+        rviz_node,
         
     ])
