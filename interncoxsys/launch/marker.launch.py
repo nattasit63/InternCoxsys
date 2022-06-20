0f170ffcb2b9d9c815c752c47e45d1778e70573c
nattasit63 <79138906+nattasit63@users.noreply.github.com>
2022-06-20 21:27:15 +0700

    launch file for aruco launch

---
+++ interncoxsys/launch/marker.launch.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir,LaunchConfiguration,FindExecutable
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+import xacro
+from launch_ros.actions import Node
+import launch_ros.actions
+from launch import LaunchDescription
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='True')
+
+    world_file_name = 'marker.world'
+
+    pkg_dir = get_package_share_directory('interncoxsys')
+
+    os.environ["GAZEBO_MODEL_PATH"] = os.path.join(pkg_dir, 'urdf')
+
+    robot_description_path =  os.path.join(pkg_dir,"urdf","model.urdf",)
+
+    robot_description = {"robot_description": xacro.process_file(robot_description_path).toxml()}
+    
+    robot_state_publisher_node = Node(package="robot_state_publisher", executable="robot_state_publisher",output="both",parameters=[robot_description],)
+
+
+    world = os.path.join(pkg_dir, 'world', world_file_name)
+   
+    gazebo = ExecuteProcess(cmd=['gazebo', '--verbose', world, '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],output='screen')
+    
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'Coconut'],
+                        output='screen')
+
+    package_description = "interncoxsys"
+
+    # RVIZ Configuration
+    rviz_config_dir = os.path.join(get_package_share_directory(
+        package_description), 'rviz', 'camera00.rviz')
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        name='rviz_node',
+        parameters=[{'use_sim_time': True}],
+        arguments=['-d', rviz_config_dir])
+    
+    sub_cam =  Node(
+            package='interncoxsys',
+            executable='sub_cam',
+            name='subCam',
+        )
+
+    sub_aruco =  Node(
+            package='interncoxsys',
+            executable='sub_auco',
+            name='subAruco',
+        )
+ 
+    return LaunchDescription([
+        
+        spawn_entity,
+        gazebo,
+        robot_state_publisher_node ,
+        rviz_node,
+        sub_cam,
+        # sub_aruco,
+    ])
