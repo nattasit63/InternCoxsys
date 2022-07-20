06d770da8986a9c8e45e990093ee2d54fcfab423
nattasit63 <79138906+nattasit63@users.noreply.github.com>
2022-07-20 20:55:52 +0700

    update vrp pick up delivery

--- MDVRP&Interface/interface/run_vrppd.py
+++ MDVRP&Interface/interface/run_vrppd.py
@@ -1,46 +1,13 @@
 from vrp import VRPPD
 
 vrppd = VRPPD()
-vrppd.open_config()
-adjMatrix = vrppd.adjacency_matrix()
 
 
-from networkx import from_numpy_matrix,relabel_nodes,DiGraph
-from numpy import array
-from vrpy import VehicleRoutingProblem
-import networkx as nx
-
-pickups_deliveries =  { 
-        "1" : [1,2,1],
-        "2" : [2,3,2],
-        "3" : [3,4,3],
-        "4" : [5,6,1],
-        "5" : [6,7,2],
-        "6" : [6,8,2],
-        "7" : [8,9,4],
-        "8" : [7,8,3],
-        "9" : [7,9,3]  }
-    
-
-
-A = array(adjMatrix,dtype=[("cost",int)])
-G = from_numpy_matrix(A,create_using=DiGraph())
-G.add_node(10)
-
-
-    
-for i in range(0,10):
-    G.add_edge(i,10,cost = 10)
-
-G = relabel_nodes(G, {0: "Source", 10: "Sink"})
-for (u, v) in pickups_deliveries.items():
-    G.nodes[v[0]]["request"] = v[1]
-    G.nodes[v[0]]["demand"] = v[2]
-    G.nodes[v[1]]["demand"] = -v[2]
-
-try:
-    prob = VehicleRoutingProblem(G, load_capacity=10, num_stops=6, pickup_delivery=True)
-    prob.solve(cspy=False)
-    print(prob.best_routes)
-except Exception as e:
-    print(e)
+def main():
+    open = vrppd.open_config()
+    adjMatrix = vrppd.adjacency_matrix()
+    solver = vrppd.vrppd_solver(max_station=7,capacity=5)
+    astar_path = vrppd.a_star_for_vehicle()
+    return astar_path
+if __name__=="__main__":
+    print(main())
--- MDVRP&Interface/interface/vrp.py
+++ MDVRP&Interface/interface/vrp.py
@@ -1,4 +1,5 @@
 import json
+from turtle import pen
 import networkx as nx
 from networkx import DiGraph
 import matplotlib.pyplot as plt
@@ -7,7 +8,7 @@ from networkx import DiGraph, from_numpy_matrix, relabel_nodes, set_node_attribu
 from numpy import array
 from vrpy import VehicleRoutingProblem
 
-G = nx.Graph()
+G = nx.DiGraph()
 
 class VRPPD():
     def __init__(self):
@@ -24,24 +25,33 @@ class VRPPD():
         self.route = []
         self.route_astar = []
         self.nodepos=[]
+        self.vrp_graph = None
+        self.backup_graph = []
+        self.best_route=dict
+        self.route_astar=[]
     def open_config(self):
         with open('/home/natta/thesis_ws/src/interface/config/example_vrppd.json','r') as f:
             self.data = json.load(f)
         self.nodes_json = self.data.get('nodes')
         self.edges_json = self.data.get('edges')
         self.depot_json = self.data.get('depot')
+        self.pickup_delivery_json = self.data.get('PICKUP_DELIVERIES')
 
         for n,p in self.nodes_json.items():
             self.nodelist.append(n)
             self.nodepos.append(p)
-            G.add_node(str(n),pos=p)
+            G.add_node(int(n),pos=p)
+        
         for s,e in self.edges_json.items():  #Add edge from yaml
-            G.add_edge(str(e[0]),str(e[1]),weight = e[2])
-            self.astar_path_cost.append(e[2])      
+            G.add_edge(int(e[0]),int(e[1]),weight = e[2])          
+            self.astar_path_cost.append(e[2])
+
+        self.G_backup = G.copy()
+
         return 1
         
     def astar(self,graph,start_node,end_node):
-        self.astar_path = nx.astar_path(graph,str(start_node),str(end_node), heuristic = None ,weight='weight')
+        self.astar_path = nx.astar_path(graph,start_node,end_node, heuristic = None ,weight='weight')
         return self.astar_path
 
     def adjacency_matrix(self):
@@ -65,7 +75,7 @@ class VRPPD():
                     path_size = len(path)
                     for k in range(path_size-1):
                         for s,e in self.edges_json.items():
-                            if(int(path[k])==e[0] and int(path[k+1])==e[1]):
+                            if(int(path[k])==int(e[0]) and int(path[k+1])== int(e[1])):
                                 sum += e[2]
                                 break
                     for s,e in self.edges_json.items():
@@ -77,3 +87,100 @@ class VRPPD():
                                     sum = e[2]
                 self.adjMatrix[i][j]= sum
         return self.adjMatrix
+    
+    def vrppd_solver(self,capacity,max_station):
+        self.adjacency_matrix()
+        A = array(self.adjMatrix,dtype=[("cost",float)])
+
+        B = from_numpy_matrix(A,create_using=DiGraph())
+
+        depot_node = int(self.nodelist[0])
+        last_node = int(self.nodelist[-1])
+        sink_node = last_node+1
+        B.add_node(sink_node)
+
+        
+        self.nodelist.append(sink_node)
+
+        for node in self.nodelist: #Connect Sink 
+            if node != sink_node:
+                B.add_edge(int(node),int(sink_node),cost=10)
+        for node in self.nodelist:
+            if int(node)!=int(depot_node):
+                B.add_edge(int(depot_node),int(node),cost=10)
+        
+        
+        B = relabel_nodes(B, {0: "Source", self.nodelist[-1]: "Sink"})
+  
+        for (u, v) in self.pickup_delivery_json.items():
+            B.nodes[v[0]]["request"] = v[1]
+            B.nodes[v[0]]["demand"] = v[2]
+            B.nodes[v[1]]["demand"] = -v[2]
+        
+        # print(G.nodes)
+        # print(G.edges)
+
+        try:
+            prob = VehicleRoutingProblem(B, load_capacity=capacity, num_stops=max_station, pickup_delivery=True)
+            prob.solve(cspy=False,pricing_strategy="Exact")
+            # print(prob.best_value)
+            self.best_route = prob.best_routes
+            return self.best_route
+        except Exception as e:
+            # print('ERROR')
+            return e
+
+
+    def a_star_for_vehicle(self):
+        depot_node = int(self.nodelist[0])
+        last_node = int(self.nodelist[-1])
+        # print(last_node)
+        sink_node = last_node
+        self.G_backup.add_node(sink_node)
+
+        for node in self.nodelist: #Connect Sink 
+            if node != sink_node:
+                self.G_backup.add_edge(int(node),int(sink_node),cost=10)
+
+        # for node in self.nodelist:
+        #     if int(node)!=int(depot_node):
+        #         self.G_backup.add_edge(int(depot_node),int(node),cost=10)
+        
+        t=[]
+        t_map=[]
+        t_route=[]
+        for s,e in self.best_route.items():
+            t.append(e)
+            t_map.append(e)
+            for i in range(0,len(t)):
+                self.route=[]
+                t1=[]
+                t2=[]
+                temp=''
+                b=[]
+                c=[]
+                len_route = len(t[i])
+                for j in range(len_route):
+                    if t[i][j] == 'Source':
+                        t[i][j] = 0
+                        t_map[i][j] = 0
+                    if t[i][j] == 'Sink':
+                        t[i][j] = self.nodelist[-1]
+                        t_map[i][j] = self.nodelist[-1]
+                for k in range(len_route-1):
+                    t1+=(self.astar(self.G_backup,t[i][k],t[i][k+1]))
+                    t2+=(self.astar(self.G_backup,t_map[i][k],t_map[i][k+1]))
+
+                for z in range(len(t1)):
+                    if t1[z]!=temp:
+                        b.append(t1[z])
+                        c.append(t1[z])
+                    temp = t1[z]
+                b[0]='Source'
+                b[-1]='Sink'
+                t_route.append(c)
+            self.route_astar.append(b)  
+
+        return self.route_astar
+
+        
